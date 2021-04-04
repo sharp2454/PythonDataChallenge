a825823de3a5e44824bbec8950bab70944e1f694
Shannan <sharpshannan60@gmail.com>
2021-04-03 21:48:50 -0500

    added code for calculation and printed results

--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -2,6 +2,14 @@
 import os
 import csv
 
+#define variables
+Candidates = []
+Votes = []
+TotalVotes = 0
+TotalPercent = []
+UniqueCandidates = []
+
+
 #create file path
 csvpath = os.path.join("Resources","election_data.csv")
 
@@ -17,7 +25,42 @@ with open (csvpath, "r") as csvfile:
     #print header
     print(f"Header: {csv_header}")
 
-#define variables
-Candidates =[]
-Votes_per_Candidate = []
+  
+
+
+
+    #read through the data after header
+    for row in csvreader:
+        
+        #total votes
+        TotalVotes += 1
+
+        #append candidates
+        if row[2] not in UniqueCandidates:
+            UniqueCandidates.append(row[2])
+
+        #make a new list
+        Votes.append(row[2])    
+
+    #read through the data and start tallying votes per candidate
+    for Candidate in UniqueCandidates:
+        Candidates.append(Votes.count(Candidates))  
+        TotalPercent.append(round(Votes.count(Candidate)/TotalVotes*100,3))  
+
+    #Return who has the most votes
+    Winner = UniqueCandidates[Candidates.index(max(Candidates))] 
+
+    #print summary
+    print("Election Results")
+    print("==========================")  
+    print(f"Total Votes: {TotalVotes}") 
+    print("==========================")
+    for x in range(len(UniqueCandidates)):
+        print(f"{UniqueCandidates[x]}: {TotalPercent[x]}% {Candidates[x]}")
+    print("==========================")
+    print(f"Winner: {Winner}")   
+    print("==========================") 
+
+
+
 
