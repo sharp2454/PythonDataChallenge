00c4e9d45a448600f6a9dff8a9bbef52ce974c60
Shannan <sharpshannan60@gmail.com>
2021-04-09 17:35:38 -0500

    changed name of output file

--- PyBank/PyBank.py
+++ PyBank/PyBank.py
@@ -7,7 +7,7 @@ Months = []
 Profits = []
 Dates= []
 
-csvpath = os.path.join("Resources","budget_data.csv")
+csvpath = os.path.join(".Resources/budget_data.csv")
 
 with open (csvpath, "r") as csvfile:
  
@@ -21,55 +21,56 @@ with open (csvpath, "r") as csvfile:
 
     #read each row of data after header
     for row in csvreader:
-        Months.append(row[0])
+        Months += 1
+        Dates.append(row[0])
         Profits.append(float(row[1]))
 
 #Set variables for analysis  
-MaxIncreaseIndex = 1
-MaxDecreaseIndex= 1
-Counter = 1
+TotalChanges = 0 
+ProfitTotal = Profits[0]
 MaxIncrease = 0.0
 MaxDecrease = 0.0
-ProfitDelta = 0.0
-ProfitDeltaTotal = 0.0
+
 
 #Run through profits list starting at 1 to calculate delta from previous month
-while Counter < (len(Profits)):
-    ProfitDelta = (Profits[Counter] - Profits[Counter - 1])
-    ProfitDeltaTotal += ProfitDelta
+for x in range (1, Months):
+    ProfitTotal += Profits[x]
+
+    CurrentChange = Profits[x] - Profits[x-1]
+    TotalChanges += CurrentChange
 
 #Find greatest increase & decrease in profits & record the index
-    if  ProfitDelta > MaxIncrease:
-        MaxIncrease = ProfitDelta
-        MaxIncreaseIndex = Counter
-    elif  ProfitDelta < MaxDecrease:
-          MaxDecrease = ProfitDelta
-          MaxDecreaseIndex = Counter
+    if  CurrentChange > MaxIncrease:
+        MaxIncrease = CurrentChange
+        MaxIncreaseDate = Dates[x]
+    elif  CurrentChange < MaxDecrease:
+          MaxDecrease = CurrentChange
+          MaxDecreaseDate = Dates[x]
 
     Counter +=1
 
 #print analysis
 print("Financial Analysis")
 print("======================================")  
-print(f"Total Months: {Counter}")
-print(f"Total Profits: ${int(sum(Profits))}")
-print(f"Average Change: ${round(ProfitDeltaTotal/(Counter - 1),2)}")
-print(f"Greatest Increase in Profits: {Months[MaxIncreaseIndex]} (${(MaxIncrease):.2f})")
-print(f"Greatest Decrease in Profits: {Months[MaxDecreaseIndex]} (${(MaxDecrease):.2f})")
+print(f"Total Months: {Months}")
+print(f"Total Profits: ${(TotalProfits):.2f}")
+print(f"Average Change: ${(TotalChanges/(Months - 1)):.2f}")
+print(f"Greatest Increase in Profits: {MaxIncreaseDate} (${(MaxIncrease):.2f})")
+print(f"Greatest Decrease in Profits: {MaxDecreaseDate} (${(MaxDecrease):.2f})")
 
 #set path for Analysis file
-output_path = 'Output.txt'
+output_path = 'Analysis.txt'
 
 #open the output path
 with open(output_path, "w") as file:
     #write analysis to the output file
     file.write("Financial Analysis\n")
     file.write("======================================\n")
-    file.write(f"Total Months: {Counter}\n")
-    file.write(f"Total Profits: ${int(sum(Profits))}\n")
-    file.write(f"Average Change: ${round(ProfitDeltaTotal/(Counter - 1),2)}\n")
-    file.write(f"Greatest Increase in Profits: {Months[MaxIncreaseIndex]} (${(MaxIncrease):.2f})\n")
-    file.write(f"Greatest Decrease in Profits: {Months[MaxDecreaseIndex]} (${(MaxDecrease):.2f})\n")
+    file.write(f"Total Months: {Months}\n")
+    file.write(f"Total Profits: ${(TotalProfits)}\n")
+    file.write(f"Average Change: ${(TotalChanges/(Months - 1)):.2f}\n")
+    file.write(f"Greatest Increase in Profits: {MaxIncreaseDate} (${(MaxIncrease):.2f})\n")
+    file.write(f"Greatest Decrease in Profits: {MaxDecreaseDate} (${(MaxDecrease):.2f})\n")
     
 
 
